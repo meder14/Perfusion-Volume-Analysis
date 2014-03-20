4b1935a0e2cbe7335704bafc4f6476c6791dcbee
meder14 <meder.14@osu.edu>
2014-03-20 12:52:21 -0400

    defined manifold dimensions and volume

--- VolumeCalc.py
+++ VolumeCalc.py
@@ -14,3 +14,10 @@ r_biosyr = 0
 l_biosyr = 0
 
 V_bio = pi*(r_biotube**2*l_biotube + r_biosyr**2*l_biosyr)
+
+# dimensions for the manifold
+r_manif = 0
+l_manif = 0  #length of main part of the manifold
+l_split = 0  #length of one of the split pieces (5 of them per manifold)
+
+V_manif = pi*r_manif**2*(l_manif + 5*l_split)
